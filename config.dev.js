b275e6380e1a4a4f8089958894ac4eb3c92bd0f5
Sanvi Singh <105363411+Jeeva-Sanvi@users.noreply.github.com>
2024-01-25 11:24:03 +0530

    Assets and gitignore added

--- config.dev.js
+++ config.dev.js
@@ -1,6 +1,6 @@
 window.envConfig = {
     sales_automation_api_url: 'http://localhost:5000',
     masterapi_url: 'http://localhost:5000',
-    base_url: 'C:/wamp64/www/sales-automation-plugin/',
+    base_url: '/Users/sanvisingh/Desktop/Work/nexus/prospect-plugin/',
     api_key: 'ddbe2c2760bafb05cdb71ee40b03f1d2'
 };
